64f56d6bd85842b9542c1c4d92b229416d9d36c2
w4wycliff <w4wycliff@gmail.com>
2019-05-08 12:20:06 +0300

    adding parcealable, intent

--- app/src/main/java/com/r/diary/CourseInfo.java
+++ app/src/main/java/com/r/diary/CourseInfo.java
@@ -10,7 +10,7 @@ import java.util.List;
  * Created by Jim.
  */
 
-public final class CourseInfo {
+public final class CourseInfo implements Parcelable{
     private final String mCourseId;
     private final String mTitle;
     private final List<ModuleInfo> mModules;
@@ -20,6 +20,15 @@ public final class CourseInfo {
         mTitle = title;
         mModules = modules;
     }
+
+
+    protected CourseInfo(Parcel source) {
+        mCourseId = source.readString();
+        mTitle = source.readString();
+        mModules = new ArrayList<>();
+        source.readTypedList(mModules, ModuleInfo.CREATOR);
+    }
+
 
     public String getCourseId() {
         return mCourseId;
@@ -76,4 +85,30 @@ public final class CourseInfo {
         return mCourseId.hashCode();
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeParcelable((Parcelable) mModules,0);
+        dest.writeString(mCourseId);
+        dest.writeString(mTitle);
+    }
+
+
+    public final static Parcelable.Creator<CourseInfo> CREATOR =
+            new Parcelable.Creator<CourseInfo>(){
+
+                @Override
+                public CourseInfo createFromParcel(Parcel source) {
+                    return new CourseInfo(source);
+                }
+
+                @Override
+                public CourseInfo[] newArray(int size) {
+                    return new CourseInfo[size];
+                }
+            };
 }
