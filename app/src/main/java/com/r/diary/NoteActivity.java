b10c893e157a23c66695fc98a58d3d87e1d31c5e
w4wycliff <w4wycliff@gmail.com>
2019-06-10 14:30:53 +0300

    add settings activity

--- app/src/main/java/com/r/diary/NoteActivity.java
+++ app/src/main/java/com/r/diary/NoteActivity.java
@@ -3,10 +3,10 @@ package com.r.diary;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
